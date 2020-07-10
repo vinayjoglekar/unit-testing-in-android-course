1dec1e58ef84adbe2dc307c13169c37e862d80cc
vinayjoglekar <vinayj1990@gmail.com>
2020-07-10 23:25:49 +0530

    #2 simple junit tests

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,43 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class StringDuplicatorTest {
+
+    public StringDuplicator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new StringDuplicator();
+    }
+
+    //name should be whattotest_i/p_expectedo/p
+    @Test
+    public void duplicate_emptyString_emptyString() {
+        String result = SUT.duplicate("");
+        Assert.assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_singleChar_doubleChars() {
+        String result = SUT.duplicate("a");
+        Assert.assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void duplicate_multiChar_doubleChars() {
+        String result = SUT.duplicate("aa");
+        Assert.assertThat(result, is("aaaa"));
+    }
+
+    @Test
+    public void duplicate_stringWithSpace_doubleOfThat() {
+        String result = SUT.duplicate("aa aa ");
+        Assert.assertThat(result, is("aa aa aa aa "));
+    }
 
 }
