eb54f1c2a13c2c5b27e99744b3c998b6a894b7a2
vinayjoglekar <vinayj1990@gmail.com>
2020-07-10 18:30:09 +0530

    simple junit tests

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test_negative_number() {
+        boolean result = SUT.isNegative(-100);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void test_zero_case() {
+        boolean result = SUT.isNegative(0);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void test_positive_case() {
+        boolean result = SUT.isNegative(1);
+        Assert.assertThat(result, is(false));
+    }
 
 }
